20d6d17bf79a2ea31655a25c4bcfa11babb4d084
Kunal Rawat <Kunalrawat399@gmail.com>
2024-05-02 01:11:35 +0530

    Basic Index Page Setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import IndexPage from './Pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h1 className=' text-4xl'>Learn React</h1>
-          <h6 className='text-sm'>Learn React</h6>
-        </a>
-      </header>
-    </div>
+    <IndexPage/>
   );
 }
 
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,10 @@
+export default function IndexPage()
+{
+    return (
+        <>
+            <div>
+                <h1 className="text-4xl text-blue-700">Home Page</h1>
+            </div>
+        </>
+    )
+}
