082989d8c1bb3c4147afcd3c5ddadcabca20e19a
Yash Verma <112695392+Yashverma849@users.noreply.github.com>
2024-05-10 14:12:41 +0530

    home page is ready

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,14 +1,17 @@
+import Homepage from "./homepage"
 import Download from "./download"
 import Contact from "./contact"
 import Features from "./Features"
 import VideoPage from "./VideoPage"
 import Header from "./Header"
+
 export default function IndexPage()
 {
     return (
         <>
             <div>
                 <Header/>
+                <Homepage/>
                 <Features/>
                 <Download/>
                 <Contact/>
