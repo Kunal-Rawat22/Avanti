55f52ff1c5355e0c3e1f592898bb6e3f2899db57
Navneet2123 <navneetkhatri002@gmail.com>
2024-05-10 12:57:46 +0530

    Header.jsx and Video Page and app logo

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,16 +1,18 @@
 import Download from "./download"
 import Contact from "./contact"
 import Features from "./Features"
-
+import VideoPage from "./VideoPage"
+import Header from "./Header"
 export default function IndexPage()
 {
     return (
         <>
             <div>
+                <Header/>
                 <Features/>
                 <Download/>
                 <Contact/>
-                
+                <VideoPage/>
                 
             </div>
         </>
